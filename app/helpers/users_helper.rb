04f4ce5408a98fa22b94f51a382707146588b063
adnanpirota <adnanpirota2011@gmail.com>
2014-10-21 19:32:16 +0000

    Me sidebar ne anen e majt

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  #Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.emaili.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.emri, class: "gravatar")
+  end
 end
