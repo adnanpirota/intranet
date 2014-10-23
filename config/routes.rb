4b139d6c8bf6976879bc9db814b60d17733bf6f1
adnanpirota <adnanpirota2011@gmail.com>
2014-10-23 13:41:29 +0000

    Me routes.rb me koment te shtum

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   
-  
+  # Komenti qe e kom shtu kur e kom fshi gabimisht routes.rb prej serverit
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
+  # Example resource route with options
   #   resources :products do
   #     member do
   #       get 'short'
