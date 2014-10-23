ba41eae1579927f1d0055048cd5a38d3f5a83172
adnanpirota <adnanpirota2011@gmail.com>
2014-10-23 20:14:21 +0000

    Komenti mbas komentit per config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :users
   
   # Komenti qe e kom shtu kur e kom fshi gabimisht routes.rb prej serverit
+  # Komenti mbas komentit
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
