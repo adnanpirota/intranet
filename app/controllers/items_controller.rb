8233565ac47a88dedd5802f97659ba0a9c30c962
adnanpirota <adnanpirota2011@gmail.com>
2014-11-18 20:16:15 +0000

    production me adapterin me postgres

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :staf_i_prokutimit
+  
   
   def show
     @user = current_user
     @item = Item.find(params[:id])
     @contract = @item.contracts
   end
+  
+  def index
+    @items = Item.all
+  end
+  
+  
+  private
+  
+  
+    def staf_i_prokutimit
+      @user = current_user
+      #puts @user.inspect
+      redirect_to(root_path) unless @user.departamenti_id == 2
+    end
   
 end
