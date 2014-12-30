db5e3d9ecb40a21ccfaf15cfe6d5128e6d849398
adnanpirota <adnanpirota2011@gmail.com>
2014-12-30 12:38:57 +0000

    pa user_i_kycyr

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,14 @@
 class StaticPagesController < ApplicationController
-  before_action :user_i_kycur
+  #before_action :user_i_kycur
   
   def home
     logger.debug "Para se me thir logged_in?"
     if logged_in?
       puts "mrenda logged_in? pra useri duhet me kon loggedin"
+      
       @user = current_user
       
+      redirect_to(help_path) unless @user.department_id == 2 || @user.department_id == 3
       #logger.debug "Mbasi qe u thir logged_in qe parimisht dmth se kemi login dhe current_user osht: #{current_user.inspect}"
       #@leave = current_user.leaves.build
     end
